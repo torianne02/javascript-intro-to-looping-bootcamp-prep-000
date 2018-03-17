e34e058deabae4e9bf4d3cdf3b42651f3a7c7696
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-03-17 17:43:14 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 function forLoop(array) {
   var output = [];
   for (var i = 0; i < 25; i++) {
-    if (i === 0) {
+    if (i === 1) {
       output.push("I am 1 strange loop.");
     }
     else {
