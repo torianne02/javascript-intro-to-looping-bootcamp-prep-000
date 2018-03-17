7dea3f01b4d3b98efa785d982ed3124ba07f006e
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-03-17 18:15:13 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -22,8 +22,10 @@ function maybeTrue() {
 }
  
 
-function doWhileLoop() {
+function doWhileLoop(array) {
   do {
-    
+    array.pop();
   }
+  while (array.length > 0 || maybeTrue())
+  return array;
 }
